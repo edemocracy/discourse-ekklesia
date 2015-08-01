dbc4fb79f0d2a4d88d73f08def55574c56d4b17f
dpausp <dpausp@posteo.de>
2015-08-01 23:31:04 +0200

    moved omniauth strategy to lib
    * preparations to separate it out into a gem

---
+++ lib/omniauth/strategies/ekklesia.rb
@@ -0,0 +1,40 @@
+module OmniAuth
+  module Strategies
+    # Strategy getting oauth2 authorization from a Ekklesia identity server.
+    # client id + secret and Ekklesia site url must be passed like:
+    #
+    # omniauth.provider(
+    #   :ekklesia,
+    #   CLIENT_ID,
+    #   CLIENT_SECRET,
+    #   client_options: { site: SITE_URL }
+    # )
+    class Ekklesia < OmniAuth::Strategies::OAuth2
+      option :client_options,
+             authorize_url: '/oauth2/authorize/',
+             token_url: '/oauth2/token/'
+
+      uid { raw_info['auid'] }
+
+      info do
+        {
+          nickname: raw_info['username']
+        }
+      end
+
+      extra do
+        {
+          raw_info: raw_info
+        }
+      end
+
+      def raw_info
+        info = access_token.get('/api/v1/user/auid/').parsed
+        info.merge!(access_token.get('/api/v1/user/profile/').parsed)
+        info.merge!(access_token.get('/api/v1/user/membership/').parsed)
+        info
+      end
+    end
+  end
+end
+
